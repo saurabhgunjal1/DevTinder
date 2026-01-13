3afd3545de8c1cd51e8343196ca603d17b76f295
Saurabh Gunjal <gunjalsaurabh16@gmail.com>
2026-01-13 16:32:19 +0530

    3 commit

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const { mongoose } = require("mongoose");
 
 async function connectDB() {
   await mongoose.connect(
-    "mongodb+srv://NamasteDev:saurabh123@namastenode.oh1dtze.mongodb.net/devTinder"
+    "mongodb+srv://NamasteDev:Saurabh321@namastenode.oh1dtze.mongodb.net/devTinder"
   );
 }
 
